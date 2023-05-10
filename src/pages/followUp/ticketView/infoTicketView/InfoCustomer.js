3e982d5acb6e736e1229a047a2b456d736dbb609
nghia <nghiait06@gmail.com>
2023-05-10 14:22:13 +0700

    update code

--- src/pages/followUp/ticketView/infoTicketView/InfoCustomer.js
+++ src/pages/followUp/ticketView/infoTicketView/InfoCustomer.js
@@ -12,74 +12,71 @@ const InfoCustomer = ({data,handleInputChange}) => {
     const dateForPicker = (dateString) => {
         return moment(new Date(dateString)).format('YYYY-MM-DD');
     };
+    const callToline1 =(valueCall)=> {
 
-    const callToline =(e)=> {
 
-       let inputValue = e.target.parentElement.parentElement.getElementsByTagName("input");
-       let PhoneLog = "";
-       if(inputValue.length >0)
-       {
-          PhoneLog = inputValue[0].value;
-       }
-    
-       if(PhoneLog.length <1)
-       {
-            Swal.fire({
-                icon: 'error',
-                title: 'Không có số điện thoại',
-                text: 'Không có số điện thoại',
-                footer: 'Yêu cầu thông tin!'
-            })
-            return;
-       }
-        let NoAgree =  data.noAgreement;
-
-        let bodySearch = {
-            phoneNumber: PhoneLog, 
-            noAgree:  NoAgree, 
-            profileId: window.location.pathname.split("/").pop()
-           
-         };
-    
-    
-         
-    ProcessingCall.MakeCall( bodySearch, (response) => {
-        if (response.statusCode === 200) {
-                  
-            Swal.fire({
-                title: 'Đang thực hiện gọi. Đang chuyển phần mềm gọi',
-                width: 600,
-                timer: 4000,
-                showConfirmButton: false,
-                padding: '3em',
-                color: '#716add',
-                background: '#fff',
-                backdrop: `
-                  rgba(0,0,123,0.4)
-                  left top
-                  no-repeat
-                `
-              });
-        } else{
-            Swal.fire({
-                icon: 'error',
-                title: 'Có lỗi xảy ra',
-                text: 'Không gọi được!',
-                footer: 'Liên hệ IT hỗ trợ'
-              })
+        // let inputValue = e.target.parentElement.parentElement.getElementsByTagName("input");
+        let PhoneLog = valueCall;
+  
+        if(PhoneLog.length <1)
+        {
+             Swal.fire({
+                 icon: 'error',
+                 title: 'Không có số điện thoại',
+                 text: 'Không có số điện thoại',
+                 footer: 'Yêu cầu thông tin!'
+             })
+             return;
         }
-        }, (error) => {
-
-
-            Swal.fire({
-                icon: 'error',
-                title: 'Có lỗi xảy ra',
-                text: 'Không gọi được!',
-                footer: 'Liên hệ IT hỗ trợ'
-              })
-        
-        });
-    }
+         let NoAgree =  data.noAgreement;
+ 
+         let bodySearch = {
+             phoneNumber: PhoneLog, 
+             noAgree:  NoAgree, 
+             profileId: window.location.pathname.split("/").pop()
+            
+          };
+     
+     
+          
+     ProcessingCall.MakeCall( bodySearch, (response) => {
+         if (response.statusCode === 200) {
+                   
+             Swal.fire({
+                 title: 'Đang thực hiện gọi. Đang chuyển phần mềm gọi',
+                 width: 600,
+                 timer: 4000,
+                 showConfirmButton: false,
+                 padding: '3em',
+                 color: '#716add',
+                 background: '#fff',
+                 backdrop: `
+                   rgba(0,0,123,0.4)
+                   left top
+                   no-repeat
+                 `
+               });
+         } else{
+             Swal.fire({
+                 icon: 'error',
+                 title: 'Có lỗi xảy ra',
+                 text: 'Không gọi được!',
+                 footer: 'Liên hệ IT hỗ trợ'
+               })
+         }
+         }, (error) => {
+ 
+ 
+             Swal.fire({
+                 icon: 'error',
+                 title: 'Có lỗi xảy ra',
+                 text: 'Không gọi được!',
+                 footer: 'Liên hệ IT hỗ trợ'
+               })
+         
+         });
+     }
+  
      return (
          <Col>
             <Form.Label>Thông tin khách</Form.Label>
@@ -102,31 +99,31 @@ const InfoCustomer = ({data,handleInputChange}) => {
             <InputGroup size="sm" className="mb-1">
                 <InputGroup.Text>Di động</InputGroup.Text>
                 <FormControl aria-label="Small" value = {data.mobilePhone}  name = "mobilePhone" onChange={handleInputChange} />
-                <InputGroup.Text className="input-group-icon"><FaPhone  onClick  = {(e)=>callToline(e)}/></InputGroup.Text>
+                <InputGroup.Text className="input-group-icon"><FaPhone  onClick  = {(e)=>callToline1(data.mobilePhone)}/></InputGroup.Text>
                 
             </InputGroup>
             <InputGroup size="sm" className="mb-1">
                 <InputGroup.Text>Khác</InputGroup.Text>
                 <FormControl aria-label="Small" value ={data.phone1} name = "phone1" onChange={handleInputChange} />
-                <InputGroup.Text className="input-group-icon"><FaPhone  onClick  = {(e)=>callToline(e)}/></InputGroup.Text>
+                <InputGroup.Text className="input-group-icon"><FaPhone  onClick  = {(e)=>callToline1(data.phone1)}/></InputGroup.Text>
                 
             </InputGroup>
             <InputGroup size="sm" className="mb-1">
                 <InputGroup.Text>Số nhà</InputGroup.Text>
                 <FormControl aria-label="Small" value = {data.houseNumber} name = "houseNumber" onChange={handleInputChange} />
-                <InputGroup.Text className="input-group-icon"><FaPhone onClick  = {(e)=>callToline(e)} /></InputGroup.Text>
+                <InputGroup.Text className="input-group-icon"><FaPhone onClick  = {(e)=>callToline1(data.houseNumber)} /></InputGroup.Text>
                 
             </InputGroup>
             <InputGroup size="sm" className="mb-1">
                 <InputGroup.Text>Văn phòng</InputGroup.Text>
                 <FormControl aria-label="Small" value = {data.officeNumber} name = "officeNumber" onChange={handleInputChange}  />
-                <InputGroup.Text className="input-group-icon"><FaPhone onClick  = {(e)=>callToline(e)} /></InputGroup.Text>
+                <InputGroup.Text className="input-group-icon"><FaPhone onClick  = {(e)=>callToline1(data.officeNumber)} /></InputGroup.Text>
                 
             </InputGroup>
             <InputGroup size="sm" className="mb-1">
                 <InputGroup.Text>Khác</InputGroup.Text>
                 <FormControl aria-label="Small" value = {data.otherPhone} name = "otherPhone" onChange={handleInputChange} />
-                <InputGroup.Text className="input-group-icon"><FaPhone  onClick  = {(e)=>callToline(e)} /></InputGroup.Text>
+                <InputGroup.Text className="input-group-icon"><FaPhone  onClick  = {(e)=>callToline1(data.otherPhone)} /></InputGroup.Text>
                 
             </InputGroup>
             <InputGroup size="sm" className="mb-1">
