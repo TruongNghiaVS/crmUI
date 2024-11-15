996c886f1abab26ea4116579694036c42ba99c8b
TruongNghia <nguyentruongnghiacntt@gmail.com>
2024-11-15 15:22:18 +0700

    fix url

--- src/pages/fololowNew/index.js
+++ src/pages/fololowNew/index.js
@@ -557,7 +557,7 @@ const  Reason = ( {triggerAutocall}) => {
           .then((result) => {
             if (result.isConfirmed) {
               var link = document.createElement('a');
-              link.href = 'http://192.168.1.141:8888' + response;
+              link.href = 'http://192.168.1.3:7777' + response;
               document.body.appendChild(link);
               link.click();
               document.body.removeChild(link);
@@ -653,7 +653,7 @@ const  Reason = ( {triggerAutocall}) => {
           .then((result) => {
             if (result.isConfirmed) {
               var link = document.createElement('a');
-              link.href = 'http://192.168.1.141:8888' + response;
+              link.href = 'http://192.168.1.3:7777' + response;
               document.body.appendChild(link);
               link.click();
               document.body.removeChild(link);
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,5 @@
 class ConstantData {
-    static DOMAIN = "http://192.168.1.141:8888";
+    static DOMAIN = "http://192.168.1.3:7777";
     static HEADERS = {
         
     };
